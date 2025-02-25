01175bf0f61b01bc70639282091e2e0b216bf040
aelasefa <aymanelasefar1337@gmail.com>
2025-02-25 15:13:52 +0000

    split all cmnd

--- main.c
+++ main.c
@@ -18,27 +18,52 @@ char	*get_path(char **env)
 	int	i;
 	char *path;
 
+	if (!env || !(*env))
+		return (NULL);
 	i = 0;
 	while (env[i])
 	{
 		if (!ft_strncmp(env[i], "PATH=", 5))
+		{
 			path = env[i];
+			return (path + 5);
+		}
 		i++;
 	}
-	return (path + 5);
+	return (NULL);
 }
 
 int	main(int ac, char **av, char **env)
 {
 	char	*path;
 	char	**split_path;
-
+	int i, j;
+	/*t_pipe *cmnd;*/
+	/*if (ac  < 5)*/
+	/*{*/
+	/*	ft_printf("Error: Invalid number of arguments.\n./pipex infile \"ls -l\" \"wc -l\" outfile\n");*/
+	/*	exit(1);*/
+	/*}*/
 	path = get_path(env);
-	split_path = ft_split(path, ':');
-	int i = 0;
-	while (split_path[i])
+	if (!path)
 	{
-		printf("%s\n",split_path[i]);
+		perror("Error\n");
+		exit(1);
+	}
+	split_path = ft_split(path, ':');
+	char **all_cmnd;
+	/*int fd1 = open("infile", O_RDONLY);*/
+	i = 1;
+	while (i < ac)
+	{
+		all_cmnd = ft_split(av[i], ' ');
+		j = 0;
+		while (all_cmnd[j])
+		{
+			printf("%s ", all_cmnd[j]);
+			j++;
+		}
+		printf("\n");
 		i++;
 	}
 }
