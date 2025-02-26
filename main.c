ee621523b5bda78860f4a18640aeff982996fd6a
aelasefa <aymanelasefar1337@gmail.com>
2025-02-26 10:33:45 +0000

    check path and execute cmd

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 /*   By: ayelasef <ayelasef@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:28:29 by ayelasef          #+#    #+#             */
-/*   Updated: 2025/02/20 10:40:36 by ayelasef         ###   ########.fr       */
+/*   Updated: 2025/02/26 10:33:23 by ayelasef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "ft_pipex.h"
 
 char	*get_path(char **env)
 {
@@ -32,6 +32,14 @@ char	*get_path(char **env)
 	return (NULL);
 }
 
+void	close_fd(int fd_in, int fd_out)
+{
+	if (fd_in != -1)
+		close(fd_in);
+	if (fd_out != -1)
+		close(fd_out);
+}
+
 int	check_access(char *cmd)
 {
 	if (access(cmd, F_OK | X_OK) == 0)
@@ -39,9 +47,65 @@ int	check_access(char *cmd)
 	return (0);
 }
 
+void	free_arr(char **arr)
+{
+	int	i;
+	if (!arr)
+		return ;
+	i = 0;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
+char	*find_path(char *cmd, char **env)
+{
+	char	**split_path;
+	char	*cmd_path;
+	char	*path;
+	int		i;
+	if (check_access(cmd))
+		return (ft_strdup(cmd));
+	path = get_path(env);
+	split_path = ft_split(path, ':');
+	if (!split_path)
+		return (NULL);
+	i = 0;
+	while (split_path)
+	{
+		cmd_path = ft_strjoin(split_path[i], cmd);
+		if (cmd_path && check_access(cmd_path))
+		{
+			free_arr(split_path);
+			return (cmd_path);
+		}
+		free(cmd_path);
+		i++;
+	}
+	free_arr(split_path);
+	return (NULL);
+}
+
 void	execute_cmd(char *cmd, char **env)
 {
+	char	**split_cmd;
+	char	*path;
 
+	split_cmd = ft_split(cmd, ' ');
+	if (!split_cmd)
+		exit(1);
+	path = find_path(split_cmd[0], env);
+	if (!path)
+	{
+		exit(1);
+	}
+	execve(path, split_cmd, env);
+	free(path);
+	free_arr(split_cmd);
+	exit(1);
 }
 
 void	process_pipex(int fd_in, char *cmd, int fd_out, char **env)
@@ -79,6 +143,12 @@ void	creat_processs(int ac, char **av, int i, char **env)
 		fd_out = fd[0];
 		i++;
 	}
+	fd_out = open(av[4], O_CREAT | O_WRONLY | O_TRUNC | 0777);
+	if (fd_out == -1)
+		exit(1);
+	process_pipex(fd_out, av[i], fd[0], env);
+	close_fd(fd_out, fd_in);
+	while (wait(NULL) > 0);
 }
 
 int	check_arg(int ac, char **av)
@@ -109,8 +179,6 @@ int	main(int ac, char **av, char **env)
 		ft_printf("./pipex <infile> <cmd1> <cmd2> <outfile>\n");
 		return (1);
 	}
-	path = get_path(env);
-	split_path = ft_split(path, ':');
 	if (pipe(fd) == -1)
 			exit(1);
 	creat_processs(ac, av, i, env);
