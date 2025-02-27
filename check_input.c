b926db466004f1ec8f5beeadb04ca6c1e0653482
aelasefa <aymanelasefar1337@gmail.com>
2025-02-27 11:44:20 +0000

    fix norm

--- check_input.c
+++ check_input.c
@@ -6,17 +6,17 @@
 /*   By: ayelasef <ayelasef@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 12:02:03 by ayelasef          #+#    #+#             */
-/*   Updated: 2025/02/26 12:02:55 by ayelasef         ###   ########.fr       */
+/*   Updated: 2025/02/26 12:13:17 by ayelasef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_pipex.h"
+#include "pipex.h"
 
 int	check_arg(int ac, char **av)
 {
-	(void)av;
 	int	total_args;
 
+	(void)av;
 	total_args = 5;
 	while (ac < total_args)
 	{
@@ -38,3 +38,22 @@ void	check_env(int ac, char **av, char **env, int i)
 	exit(2);
 }
 
+char	*get_path(char **env)
+{
+	int		i;
+	char	*path;
+
+	if (!env || !(*env))
+		return (NULL);
+	i = 0;
+	while (env[i])
+	{
+		if (!ft_strncmp(env[i], "PATH=", 5))
+		{
+			path = env[i];
+			return (path + 5);
+		}
+		i++;
+	}
+	return (NULL);
+}
