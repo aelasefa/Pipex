f12bfb7302b65cb9598aaf22523d893252157eb2
aelasefa <aymanelasefar1337@gmail.com>
2025-02-27 11:46:02 +0000

    add prototype

--- bonus/pipex_bonus.h
+++ bonus/pipex_bonus.h
@@ -10,31 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef FT_PIPEX_BONUS_H
-# define FT_PIPEX_BONUS_H
-#include "../ft_printf/ft_printf.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
-#include <fcntl.h>
-typedef struct s_pipe 
-{
-	char *infile;
-	char **cmd;
-	char *outfile;
-}		t_pipe;
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+# include "../ft_printf/ft_printf.h"
+# include <fcntl.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
 
-//ft_strjoin
-char	*ft_strdup(char *s1);
-char	*ft_strjoin(char *s1, char *s2);
-//check_input
-int	check_arg(int ac, char **av);
-void	check_env(int ac, char **av, char **env, int i);
-//pipex_utils
-void	close_fd(int fd_in, int fd_out);
-int	check_access(char *cmd);
-void	free_arr(char **arr);
-void ft_perror(char *error);
+typedef struct s_pipe
+{
+	char	*infile;
+	char	**cmd;
+	char	*outfile;
+}			t_pipe;
+
+// ft_strjoin
+char		*ft_strdup(char *s1);
+char		*ft_strjoin(char *s1, char *s2);
+// check_input
+int			check_arg(int ac, char **av);
+void		check_env(int ac, char **av, char **env, int i);
+char		*get_path(char **env);
+// pipex_utils
+void		close_fd(int fd_in, int fd_out);
+int			check_access(char *cmd);
+void		free_arr(char **arr);
+void		ft_perror(char *error);
+char		*find_path(char *cmd, char **env);
 #endif
