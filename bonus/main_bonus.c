1abbfb8214e8702482f1a39e4196f8cbde83a7e5
aelasefa <aymanelasefar1337@gmail.com>
2025-03-04 04:44:23 +0000

    same change in mandatory main

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -46,33 +46,32 @@ void	process_pipex(int fd_in, char *cmd, int fd_out, char **env)
 		ft_perror("pid failed");
 	if (pid == 0)
 	{
-		close(fd_in);
 		if (dup2(fd_out, 1) == -1)
 			ft_perror("dup2 failed");
 		close(fd_out);
-		execute_cmd(cmd, env);
-		exit(0);
-	}
-	else {
-		close(fd_out);
-		dup2(fd_in, 0);
+		if (dup2(fd_in, 0) == -1)
+			exit(1);
 		close(fd_in);
+		execute_cmd(cmd, env);
+		exit(7);
 	}
 }
 
 void	creat_processs(int ac, char **av, int i, char **env)
 {
-	int fd[2], fd_in, (fd_out);
+	int (fd_in), (fd_out), fd[2];
 	fd_in = open(av[1], O_RDONLY);
-	dup2(fd_in, 0);
-	i++;
 	if (fd_in == -1)
-		ft_printf("input file error");
+		ft_putstr_fd("input file error\n", 2);
 	while (i < ac - 2)
 	{
 		if (pipe(fd) == -1)
 			ft_perror("Pipe failed");
-		process_pipex(fd[0], av[i], fd[1], env);
+		process_pipex(fd_in, av[i], fd[1], env);
+		close(fd[1]);
+		if (fd_in != -1)
+			close(fd_in);
+		fd_in = fd[0];
 		i++;
 	}
 	fd_out = open(av[ac - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
